b350ab847d7a9638daa864e35c743e9beaa92130
Christian Kelsom-Martin <github@ekahi.co>
2019-09-23 17:43:44 -0700

    added remove contributor

--- public/assets/js/board.js
+++ public/assets/js/board.js
@@ -33,7 +33,9 @@ function getBoard(id) {
     board = data;
     renderBoard();
   }).catch(function(err) {
-    location.replace('/boards');
+    if (err.statusText === 'Unauthorized') {
+      location.replace('/boards');
+    }
   });
 }
 
@@ -114,7 +116,14 @@ function renderBoard() {
 
 function renderContributors() {
   let $contributorListItems = board.users.map(function(user) {
-    let $contributorListItem = $('<li>').text(user.email);
+    let $contributorListItem = $('<li>');
+    let $contributorSpan = $('<span>').text(user.email);
+    let $contributorDeleteButton = $('<button class="danger">Remove</button>')
+      .data(user)
+      .on('click', handleContributorDelete);
+
+    $contributorListItem.append($contributorSpan, $contributorDeleteButton);
+
     return $contributorListItem;
   });
 
@@ -403,6 +412,22 @@ function openContributorModal() {
   displayMessage('');
 
   MicroModal.show('contribute');
+}
+
+function handleContributorDelete() {
+  let { id, email } = $(event.target).data();
+
+  $.ajax({
+    url: '/api/user_boards',
+    method: 'DELETE',
+    data: {
+      user_id: id,
+      board_id: board.id
+    }
+  }).then(function() {
+    init();
+    displayMessage(`Successfully removed user: ${email}`, 'success');
+  });
 }
 
 $contributorModalSaveButton.on('click', handleContributorSave);
